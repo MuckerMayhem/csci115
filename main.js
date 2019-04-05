3cdb50268a286cd8816978c37d9292dca8218e81
Cole <continumconstil@gmail.com>
2019-04-04 17:31:20 -0700

    comments

--- main.js
+++ main.js
@@ -5,20 +5,24 @@ var clickButton = (function () {
     return function (id) {
         console.log("counter is " + counter);
 
+        // run this chunk if we hit an active button.
         if (document.getElementById(id).innerHTML === "ACTIVE") {
             document.getElementById((id).innerHTML = "Button");
+
+            // increase the counter by whatever the dynamic value is set too.
             counter = counter + parseInt(document.getElementById("pph").value);
             console.log("hit active button.");
-            //document.getElementById("pph").value =
             document.getElementById("points").innerHTML = "" + counter;
 
             console.log("time is: " + time);
+            // increase speed of changing buttons once we hit 5 or more points.
             if (document.getElementById("points").innerHTML >= 5) {
                 clearInterval(t);
-
+                //decrease time it takes for a button to switch without being hit every time we hit an active button.
                 time = time - document.getElementById("points").innerHTML * document.getElementById("speed").value;
                 t = setInterval(setNextButton, time);
             }
+            // reset the interval after each successful hit
             setNextButton();
             clearInterval(t);
             t = setInterval(setNextButton, time);
@@ -31,8 +35,11 @@ var setNextButton = (function () {
     var button = 0;
     var lastButton = -1;
     return function () {
+        // if lastButton = -1, then this is the first time a button was hit in this instance.
+        // we keep track of this so we can make sure the same button isn't chosen twice in a row.
         if (lastButton !== -1) document.getElementById("but" + button).innerHTML = "Button";
 
+        // choose a number between 0 and 15.
         button = Math.floor(Math.random() * 16);
         if (lastButton === button) button++;
 
@@ -40,6 +47,7 @@ var setNextButton = (function () {
 
         lastButton = button;
         console.log(button);
+        //set the chosen button as the active button.
         document.getElementById("but" + button).innerHTML = "ACTIVE";
     }
 
